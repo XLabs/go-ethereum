bd851154ac9556a7dec86498b539a9ee92d72988
Mariano <sostomariano@gmail.com>
2025-04-21 17:58:07 -0300

    change logging implementation

--- eth/tracers/live/xlabstracer.go
+++ eth/tracers/live/xlabstracer.go
@@ -7,24 +7,40 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"net"
-	"path/filepath"
-
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/eth/tracers"
 	"github.com/ethereum/go-ethereum/rlp"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"log"
+	"net"
+	"os"
 )
-
-func init() {
-	tracers.LiveDirectory.Register("xlabstracer", newXlabsTracer)
-}
 
 type xlabsTracerConfig struct {
 	SocketFilePath string `json:"socketFilePath"` // Path to the unix-domain-socket
 	LogFile        string `json:"path"`           // Path to the directory where the tracer logs will be stored
 	MaxSize        int    `json:"maxSize"`        // MaxSize is the maximum size in megabytes of the tracer log file before it gets rotated. It defaults to 100 megabytes.
+}
+
+type xlabsTracer struct {
+	conn              net.Conn
+	logger            *log.Logger
+	txReceipts        []*types.Receipt
+	currentBlockEvent *tracing.BlockEvent
+	ctx               context.Context
+	cancelFunc        context.CancelFunc
+	logFile           *os.File
+}
+
+type Event struct {
+	LatestBlock    *types.Header
+	FinalizedBlock *types.Header
+	SafeBlock      *types.Header
+	Receipts       []*types.Receipt
+}
+
+func init() {
+	tracers.LiveDirectory.Register("xlabstracer", newXlabsTracer)
 }
 
 func newXlabsTracer(cfg json.RawMessage) (*tracing.Hooks, error) {
@@ -36,26 +52,23 @@ func newXlabsTracer(cfg json.RawMessage) (*tracing.Hooks, error) {
 		return nil, errors.New("xlabstracer output path is required")
 	}
 
-	logger := &lumberjack.Logger{
-		// Store traces in a rotating file
-		Filename: filepath.Join(config.LogFile, "xlabstracer.jsonl"),
+	// Set up event logger
+	logFile, err := os.OpenFile(config.LogFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("opening geth.log: %v", err)
 	}
 
-	if config.MaxSize > 0 {
-		logger.MaxSize = config.MaxSize
-	}
+	logger := log.New(logFile, "", log.LstdFlags)
 
 	if config.SocketFilePath == "" {
 		config.SocketFilePath = "/tmp/trace.sock"
-		_, err := logger.Write([]byte(fmt.Sprintf("socket file path: %s", config.SocketFilePath)))
-		if err != nil {
-			return nil, fmt.Errorf("xlabstracer init error: failed to write socket file path to log. error:%s", err.Error())
-		}
+		logger.Printf("socket file path: %s", config.SocketFilePath)
 	}
 
 	// Open the unix-domain-socket
 	conn, err := net.Dial("unix", config.SocketFilePath)
 	if err != nil {
+		logger.Printf("failed to connect: %v", err)
 		return nil, fmt.Errorf("xlabstracer init error: failed to connect to unix-domain-socket. error:%s", err.Error())
 	}
 
@@ -64,6 +77,7 @@ func newXlabsTracer(cfg json.RawMessage) (*tracing.Hooks, error) {
 	t := &xlabsTracer{
 		conn:       conn,
 		logger:     logger,
+		logFile:    logFile,
 		ctx:        ctx,
 		cancelFunc: cancelFunc,
 	}
@@ -76,23 +90,8 @@ func newXlabsTracer(cfg json.RawMessage) (*tracing.Hooks, error) {
 	}, nil
 }
 
-type xlabsTracer struct {
-	conn              net.Conn
-	logger            *lumberjack.Logger
-	txReceipts        []*types.Receipt
-	currentBlockEvent *tracing.BlockEvent
-	ctx               context.Context
-	cancelFunc        context.CancelFunc
-}
-
-type Event struct {
-	LatestBlock    *types.Header
-	FinalizedBlock *types.Header
-	SafeBlock      *types.Header
-	Receipts       []*types.Receipt
-}
-
 func (s *xlabsTracer) onBlockStart(ev tracing.BlockEvent) {
+	s.logger.Printf("block start: %v\n", ev)
 	s.currentBlockEvent = &ev
 }
 
@@ -124,21 +123,23 @@ func (s *xlabsTracer) onBlockEnd(err error) {
 func (s *xlabsTracer) sendUDSMessage(payload Event) {
 	var buf bytes.Buffer
 	if err := rlp.Encode(&buf, payload); err != nil {
-		s.logger.Write([]byte(fmt.Sprintf("Error encoding payload: %v\n", err)))
+		s.logger.Printf("Error encoding payload: %v\n", err)
 		return
 	}
 
 	// prefix with 4-byte length to frame messages
 	length := uint32(buf.Len())
 	if err := binary.Write(s.conn, binary.BigEndian, length); err != nil {
-		s.logger.Write([]byte(fmt.Sprintf("Error writing message length: %v\n", err)))
+		s.logger.Printf("Error writing message length: %v\n", err)
 		return
 	}
 
 	_, err := s.conn.Write(buf.Bytes())
 	if err != nil {
-		s.logger.Write([]byte(fmt.Sprintf("Error writing message to socket: %v\n", err)))
+		s.logger.Printf("Error writing message to socket: %v\n", err)
 	}
+
+	s.logger.Printf("Message sent successfully: blockNumber:%d\n", payload.LatestBlock.Number)
 }
 
 func (s *xlabsTracer) OnTxEnd(receipt *types.Receipt, err error) {
@@ -153,8 +154,9 @@ func (s *xlabsTracer) cleanUp() {
 }
 
 func (s *xlabsTracer) onClose() {
+	s.logger.Printf("xlabstracer closing\n")
 	s.cleanUp()
 	s.cancelFunc()
 	s.conn.Close()
-	s.logger.Close()
+	s.logFile.Close()
 }
